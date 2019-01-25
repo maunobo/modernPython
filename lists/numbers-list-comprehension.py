a2500474d96bb32e891b200edc89ac3494f5b07e
Maurice Noel Bouniol <mnb.dev.in@gmail.com>
2019-01-25 22:17:44 +0100

    added nestes lists
    added nestes lists

--- lists/numbers-list-comprehension.py
+++ lists/numbers-list-comprehension.py
@@ -1,4 +1,4 @@
-print('*** Numbers ***')
+aprint('*** Numbers ***')
 numbers = [1,2,3,4,5]
 
 [x*2 for x in numbers]
@@ -7,9 +7,9 @@ print(numbers)
 print([x*2 for x in numbers])
 
 print('*** Names ***')
-name = 'noel'
-print(name)
-print([char.upper() for char in name])
+myName = 'noel'
+print(myName)
+print([char.upper() for char in myName])
 
 
 print('*** Friends ***')
@@ -50,4 +50,52 @@ print('Evens: ', evens) # [2,4,6]
 print('Odds: ', odds) # [1,3,5]
 
 evenOdd = [num*2 if num % 2 == 0 else num/2 for num in numbers] # [0.5, 4, 1.5, 8, 2.5, 12]
-print('Even numbers multiplied by 2, odd numbers divided by 2:',evenOdd)
+print('Even numbers multiplied by 2, odd numbers divided by 2:',evenOdd) 
+
+
+print('*** Vowels ***')
+with_vowels = 'This is too much fun!'
+''.join(char for char in with_vowels if char not in "aeiou")
+print(with_vowels)
+print(''.join(char for char in with_vowels if char not in "aeiou"))
+# 'Ths s s mch fn!'
+
+print('*** In - Number Lists ***')
+list1 = [1,2,3,4]
+list2 = [3,4,5,6]
+answerNumListIn = [num for num in list1 if num in list2]
+print(answerNumListIn)
+## [3,4] 
+
+print('*** Reverse + lower() - Name Lists ***')
+names = ['Elie', 'Tim', 'Matt']
+print(names)
+answerNameList = [name[::-1].lower() for name in names]
+print(answerNameList)
+
+print('*** Mod - Number Lists ***')
+numbersMod = list(range(1,101))
+# print(numbersMod) # [1,2,3,...99,100]
+answerNumbersMod = [num for num in numbersMod if num%12==0]
+print(answerNumbersMod)
+# [12, 24, 36, 48, 60, 72, 84, 96]
+
+print('*** ''.join - String ***')
+amaze = 'amazing'
+answerJoin =  [''.join(char) for char in amaze if char not in 'aeiou']
+# answerJoin = [char for char in "amazing" if char not in ["a", "e", "i", "o", "u"]
+print(answerJoin)
+# ['m', 'z', 'n', 'g']
+
+print('*** Nested Number List ***')
+nested_number_list = [[1,2,3],[4,5,6],[7,8,9]]
+[[print(val) for val in l] for l in nested_number_list]
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
